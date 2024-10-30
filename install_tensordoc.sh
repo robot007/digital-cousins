5115e12ad69441a5bca32fc141984d1328392ca8
Zhen Song <zhenieee@gmail.com>
2024-10-30 05:10:44 +0000

    installed

--- install_tensordoc.sh
+++ install_tensordoc.sh
@@ -2,4 +2,21 @@
 # python 
 cd ~/
 # git clone https://github.com/robot007/digital-cousins.git
+# git fetch origin zs
+# git checkout 
+sudo apt-get install cmake
+git clone https://github.com/StanfordVL/egl_probe.git
+cd egl_probe
+git checkout -b install 3ddf90db69264de2c621
+# from 
+cp -f ../tmp/egl_prob.setup.py ./setup.py
+pip install -e .
+pip install --no-cache-dir --force-reinstall egl_probe
+
+
+conda activate acdc
+# in conda environment, reinstal pytorch 2.1.0 to match CUDA 12.2
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+
 ./install.sh -e acdc -c $(readlink -f /usr/local/cuda) -m 
+# press Y 2 times
